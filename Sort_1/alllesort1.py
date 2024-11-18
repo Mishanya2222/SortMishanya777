86181df9d5f36e4fd6f1b9f855591239631cb8d8
Tutor <Tutor@main.nsu.ru>
2024-11-18 14:03:06 +0700

    сделал всё

--- Sort_1/alllesort1.py
+++ Sort_1/alllesort1.py
@@ -34,14 +34,48 @@ print()
 
 
 
-oporn=a[random.randint(0, t-1)]
-def qsort(a):
-    m=[]
-    n=[]
-    for i in a:
-        if oporn >= i:
-            m.append(i)
-        elif oporn<i:
-            n.append(i)
-            
+# oporn=a[random.randint(0, t-1)]
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+def partition(arr, low, high):
+    pivot = arr[low]
+    i = low + 1
+    for j in range(low + 1, high + 1):
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[low], arr[i - 1] = arr[i - 1], arr[low]
+    return i - 1
+x = int(len([1,2,3,4,54,6,7,8,47,2,52,545,24,]))-1
+print(quick_sort([1,2,3,4,54,6,7,8,47,2,52,545,24,],0, x))
+
+
+
+
+
+def selection_sort(a):
+    for i in range(len(a) - 1):
+        imin = i
+        for j in range(i + 1, len(a)):
+            if a[j] < a[imin]:
+                imin = j
+        a[i], a[imin] = a[imin], a[i]
+
+
+
+
+
+
+
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        tmp = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > tmp:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = tmp
 
